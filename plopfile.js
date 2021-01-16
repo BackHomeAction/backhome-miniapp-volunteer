c11e200216411fab11bf272710c1b6918469293d
fly3949 <i@fly.moe>
2021-01-16 12:12:01 +0800

    feat: Plop

---
+++ plopfile.js
@@ -0,0 +1,26 @@
+module.exports = function (plop) {
+  plop.setGenerator("page", {
+    description: "generate a page",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "Please input page name:",
+      },
+    ],
+    actions: (data) => {
+      const name = "{{name}}";
+      const actions = [
+        {
+          type: "add",
+          path: `src/pages/${name}/index.vue`,
+          templateFile: "plop-templates/page/index.hbs",
+          data: {
+            name: name,
+          },
+        },
+      ];
+      return actions;
+    },
+  });
+};
