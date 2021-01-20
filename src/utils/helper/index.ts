dfe69ce980beda5f3a33d1db6f82072a8a9da02b
fly3949 <i@fly.moe>
2021-01-20 19:07:48 +0800

    feat: 请求组件处理业务错误

--- src/utils/helper/index.ts
+++ src/utils/helper/index.ts
@@ -1,22 +1,30 @@
-export const navigateTo = (url: string): void => {
+export const navigateTo = (url: string) => {
   uni.navigateTo({
     url,
   });
 };
 
-export const navigateBack = (delta: number = 1): void => {
+export const navigateBack = (delta: number = 1) => {
   uni.navigateBack({ delta });
 };
 
 export const showToast = (
   title: string,
   icon: "success" | "loading" | "none" | undefined = "none"
-): void => {
+) => {
   uni.showToast({ title, icon });
 };
 
-export const switchTab = (url: string): void => {
+export const switchTab = (url: string) => {
   uni.switchTab({
     url,
+  });
+};
+
+export const showModalError = (content = "未知错误") => {
+  uni.showModal({
+    title: "错误",
+    content,
+    showCancel: false,
   });
 };
--- src/utils/request/index.ts
+++ src/utils/request/index.ts
@@ -1,5 +1,32 @@
+import { showModalError } from "../helper";
 import Request from "./lib";
 
-const http = new Request();
+const http = new Request({
+  baseURL: "http://101.200.120.207:8080/",
+});
+
+http.interceptors.response.use(
+  (response: any) => {
+    /* 对响应成功做点什么 可使用 async await 做异步操作 */
+
+    const state = response.data.state;
+    const message = response.data.message;
+
+    if (state !== 200) {
+      if (state !== 103) {
+        /* 除了 token 过期以外的错误 */
+        showModalError(message);
+      }
+      return Promise.reject(response);
+    }
+
+    return response;
+  },
+  (response: any) => {
+    /*  对响应错误做点什么 （statusCode !== 200）*/
+
+    return Promise.reject(response);
+  }
+);
 
 export default http;
